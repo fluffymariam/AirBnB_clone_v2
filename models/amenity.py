39e1b95d6ecfc9c72d7092ab78bcc1980e19c86a
fluffymariam <adefowobiladejobi@gmail.com>
2023-12-23 15:11:43 -0800

    p

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -7,7 +7,10 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class"""
+    """This is de class fr Amenity
+    Attributes:
+        name: input name
+    """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+import shlex
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade='all, delete, delete-orphan',
+        backref="state"
+    )
+
+    @property
+    def cities(self):
+        all_models = models.storage.all()
+        cities_list = [
+            model for model in all_models.values() if isinstance(model, City)
+        ]
+        matching_cities = [
+            city for city in cities_list if city.state_id == self.id
+        ]
+        return matching_cities
